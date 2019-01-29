b6bed3527b6d9e9c1267f7b88c2af493343110c7
lpj <200814174@qq.com>
2019-01-29 15:13:42 +0800

    new design

---
+++ src/app.js
@@ -0,0 +1,74 @@
+import './stylus/index.styl'
+import {data} from './data/data.js'
+
+
+Vue.prototype.addClass = function(dom, classname){
+	if(dom.className) {
+		dom.className = dom.className + ' ' + classname
+	} else{
+		dom.className = classname
+	}
+}
+
+
+var app = new Vue({
+	el: '#app',
+	data() {
+		return {
+			menu: data['menu'],
+			header: data['header'],
+			server: data['server'](),
+			process: data['process'](),
+			about: data['about'](),
+			contact: data['contact'](),
+			footer: data['footer'](),
+			heightArr: [],
+			mStand: 'stand-1',
+			SCROLL_TOP: 0
+		}
+	},
+	beforeCreate() {
+
+	},
+	mounted() {
+		// var self = this
+		this.addClass(this.$refs.banner, 'Animated')
+		var Refs = this.$refs
+		var heightArr = []
+		Object.keys(Refs).forEach(function(key, index){
+			heightArr[index] = (Refs[key].offsetHeight - 50) + (!!heightArr[index-1] ? heightArr[index-1] : 0)
+
+		})
+		this.heightArr = heightArr
+		window.addEventListener('scroll', () => {
+			this.SCROLL_TOP = document.documentElement.scrollTop || document.body.scrollTop || window.pageYOffset
+		}, true)
+
+		console.log(this.heightArr)
+	},
+	methods: {
+		scrollHandler() {
+			
+		},
+		MenuHover(i) {
+			this.mStand = 'stand-'+(i+1)
+		}
+	},
+	watch: {
+		SCROLL_TOP(n, o) {
+			// console.log(n, o)
+			if(n < this.heightArr[0]){
+				console.log(n + ' <' + this.heightArr[0])
+				this.mStand = 'stand-1'
+			}else if(n < this.heightArr[1]){
+				this.mStand = 'stand-2'
+			}else if(n < this.heightArr[2]){
+				this.mStand = 'stand-3'
+			}else if(n < this.heightArr[3]){
+				this.mStand = 'stand-4'
+			}else{
+				this.mStand = 'stand-5'
+			}
+		}
+	}
+})
---
+++ src/data/data.js
@@ -0,0 +1,123 @@
+
+var menu = [
+		{
+			title: '首页'
+		},
+		{
+			title: '开发服务',
+			description: 'development service'
+		},
+		{
+			title: '项目开发流程',
+			description: 'project development process'
+		},
+		{
+			title: '关于我们',
+			description: 'about caiton network'
+		},
+		{
+			title: '联系我们',
+			description: 'contact us'
+		}
+	]
+
+export var data = {
+	menu: menu,
+	header: [
+		{
+			des_cn: '专注于互联网开发',
+			des_en: 'focus on internet development'
+		},
+		{
+			des_cn: '专业铸造品质',
+			des_en: 'professional casting quality'
+		},
+		{
+			des_cn: '为您提供最全的技术服务',
+			des_en: 'to provide you with the most comprehensive<br/> technical service'
+		}
+	],
+	server: function(){
+		var serObj = {}
+		serObj.head = menu[1]
+		serObj.list = [
+			{
+				title: '官网开发',
+				description: ''
+			},
+			{
+				title: '手机端开发',
+				description: ''
+			},
+			{
+				title: '微信公众号开发',
+				description: ''
+			},
+			{
+				title: '小程序开发',
+				description: ''
+			}
+		]
+		serObj.des = [
+			'CAITON 为您提供',
+			'最有价值的解决方案'
+		]
+		return serObj
+	},
+	process: function(){
+		var proObj = {}
+		proObj.head = menu[2]
+		proObj.list = [
+			{
+				title: '原型开发',
+				description: ''
+			},
+			{
+				title: 'UI设计',
+				description: ''
+			},
+			{
+				title: '技术研发',
+				description: ''
+			},
+			{
+				title: '测试上线',
+				description: ''
+			},
+			{
+				title: '维护服务',
+				description: ''
+			}
+		]
+		return proObj
+	},
+	about: function(){
+		var aboutObj = {}
+		aboutObj.head = menu[3]
+		aboutObj.content = {
+			name_cn: '珠海财通网络有限公司',
+			name_en: 'Zhuhai Caiton Network Co., Ltd.',
+			logo: '../images/logo-big.png',
+			description: '财通网络2018年创立于美丽的滨海城市，是一家专注于网络产品开发的软件公司，如果你刚好有网络开发的需求，可以通过下面的联系方式联系我们，我们将为你提供专业的网络开发服务。'
+		}
+		return aboutObj
+	},
+	contact: function() {
+		var contentObj = {}
+		contentObj.head = menu[4]
+		contentObj.content = {
+			'Wechat': 'lipeijian2202',
+			'QQ': '200814174',
+			'Email': 'lpjsteel@hotmail.com'
+		}
+		return contentObj
+	},
+	footer: function() {
+		var footerObj = {}
+		footerObj.menu = menu.slice(1)
+		footerObj.copyright = 'Copyright © 2018-'+ new Date().getFullYear() +' caiton 财通网络'
+
+		return footerObj
+	}
+
+}
